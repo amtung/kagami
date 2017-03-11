bc55f533e731896cce5a94d2699f222798a7e060
Samantha Bretous <sam@samanthabretous.com>
2017-03-11 18:14:38 -0500

    render svg weather icons based on weather conditions

--- frontend/weather/displayWeatherInfo.jsx
+++ frontend/weather/displayWeatherInfo.jsx
@@ -1,19 +1,35 @@
-var React = require('react')
-var DisplayWeatherInfo = ({ weather }) => (
-  <div>
-   <h3>{weather.name}</h3>
-   <div>
-     {/*icon function will go here*/}
-     <div>
-       <h1>{Math.round(weather.main.temp)}</h1>
-       <p>{
-        `${Math.round(weather.main.temp_min)} |
-        ${Math.round(weather.main.temp_max)}`
-      }</p>
-     </div>
-   </div>
-   <p>{weather.weather[0].description}</p>
-  </div>
-);
+const React = require('react');
+const weatherIcons = {
+  'clear sky': require('./weather_icons/Sun.inline.svg'),
+  'few clouds': require('./weather_icons/Cloud-Sun.inline.svg'),
+  'scattered clouds': require('./weather_icons/Cloud-Sun.inline.svg'),
+  'broken clouds': require('./weather_icons/Cloud-Sun.inline.svg'),
+  'shower rain': require('./weather_icons/Cloud-Rain.inline.svg'),
+  'rain': require('./weather_icons/Cloud-Rain-Sun.inline.svg'),
+  'thunderstorm': require('./weather_icons/Cloud-Lightning.inline.svg'),
+  'snow': require('./weather_icons/Cloud-Snow-Alt.inline.svg'),
+  'mist': require('./weather_icons/Cloud-Fog.inline.svg'),
+  'overcast clouds': require('./weather_icons/Cloud-Fog.inline.svg')
+}
+
+const DisplayWeatherInfo = ({ weather }) => {
+  const Special = weatherIcons[weather.weather[0].description]
+  return (
+    <div className="weather">
+      <h3>{weather.name}</h3>
+      <div className="mainWeather">
+        <Special className="weather_icon"/>
+        <div className="tempContainer">
+          <h1>{Math.round(weather.main.temp)}</h1>
+          <p>{
+          `${Math.round(weather.main.temp_min)} |
+          ${Math.round(weather.main.temp_max)}`
+          }</p>
+        </div>
+      </div>
+      <p>{weather.weather[0].description}</p>
+    </div>
+  );
+};
 
 module.exports = DisplayWeatherInfo;
